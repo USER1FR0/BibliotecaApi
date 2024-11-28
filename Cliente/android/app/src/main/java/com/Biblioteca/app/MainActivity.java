e5b452b47658e8895a2c807d9830594b82a07571
SekiroK5 <arin01246@gmail.com>
2024-11-27 20:24:47 -0600

    Ya funciona bien

---
+++ Cliente/android/app/src/main/java/com/Biblioteca/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Biblioteca.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ Cliente/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Biblioteca.app',
+  appName: 'BiblioUTNG',
+  webDir: 'dist/sgca/browser'
+};
+
+export default config;
