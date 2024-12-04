d9480d4e4039265d66f989738442e56d3445268c
SekiroK5 <arin01246@gmail.com>
2024-12-03 20:15:38 -0600

    .asd

--- Cliente/src/app/components/home/enviroments/enviroment.ts
+++ Cliente/src/app/components/home/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     hereApiKey: 'EA1yweP2Qasi_jTOqeiKhRoBDqnu_Oh_QKiG91e2d38',
-    apiUrl: 'http://localhost:4200/home', // Ajusta según tu configuración local
+    apiUrl: 'https://bibliotecaapi-1-ru9q.onrender.com/home', // Ajusta según tu configuración local
     paypalClientId: 'AYTdANq0qL6-mWz_0eyqAnEikHC3VX26c67xjAuDkFX0mt5yx3gMOCsUuTpBRjd6TsFv3jwD4NNOL7Ho'
   };
