4ea02dcbb04a65cedb0c9ddf7d1f09b7eabd56f1
SekiroK5 <arin01246@gmail.com>
2024-12-03 20:09:31 -0600

    corregido

--- Cliente/src/app/components/home/enviroments/enviroment.prod.ts
+++ Cliente/src/app/components/home/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     hereApiKey: 'EA1yweP2Qasi_jTOqeiKhRoBDqnu_Oh_QKiG91e2d38',
-    apiUrl: 'https://bibliotecaapi-1-ru9q.onrender.com/homei', // Ajusta según tu URL de producción
+    apiUrl: 'https://bibliotecaapi-1-ru9q.onrender.com/home', // Ajusta según tu URL de producción
     paypalClientId: 'AYTdANq0qL6-mWz_0eyqAnEikHC3VX26c67xjAuDkFX0mt5yx3gMOCsUuTpBRjd6TsFv3jwD4NNOL7Ho'
   };
